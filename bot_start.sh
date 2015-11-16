7f3811e711b61a1d5c6e4901d3458deda41a3f17
Marc Palmer <marc@anyware.co.uk>
2015-11-16 14:06:57 +0000

    Added comment

--- bot_start.sh
+++ bot_start.sh
@@ -2,4 +2,6 @@
 echo "Botlane starting..."
 PATH="$PATH:/usr/local/bin"
 cd "${XCS_SOURCE_DIR}/${PROJECT_ROOT}"
+
+# TODO: Emit a friendly error and exit status if fastlane not found or Fastfile missing 
 fastlane bot_start
