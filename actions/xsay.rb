f40660ae62f6586bfc1d4609a5161ea7107b3684
Marc Palmer <marc@anyware.co.uk>
2015-11-20 12:02:11 +0000

    Added "dont_speak" and test lane

--- actions/xsay.rb
+++ actions/xsay.rb
@@ -9,25 +9,42 @@ module Fastlane
                                        description: "The message to speak"),
           FastlaneCore::ConfigItem.new(key: :voice,
                                        env_name: "XSAY_VOICE",
+                                       optional: true,
                                        description: "The voice to use",
-                                       is_string: true)
+                                       is_string: true),
+          FastlaneCore::ConfigItem.new(key: :dont_speak,
+                                       env_name: "XSAY_DONTSPEAK",
+                                       is_string: false,
+                                       description: "Set to true to suppress all speech",
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                         raise "Please pass a valid value for dont_speak. Use one of the following: true, false" unless value.kind_of?(TrueClass) || value.kind_of?(FalseClass)
+                                       end)
         ]
       end
-
+ 
 
 
       def self.run(params)
+        dontSpeak = ENV["XSAY_DONTSPEAK"] == "true" ? true : false
+        
         text = params[:message] if params.kind_of?(Hash)
         if text
             text = text.to_s
             voice = params[:voice]
+
+            if params[:dont_speak]
+              dontSpeak = params[:dont_speak]
+            end
         else
             text = params.join(' ') if params.kind_of?(Array) # that's usually the case
             text = params if params.kind_of?(String)
             
             voice = ENV["XSAY_VOICE"]
         end
-  
+
+        return if dontSpeak
+
         raise "You can't call the `say` action as OneOff" unless text
         
         text = text.tr("'", '"')
